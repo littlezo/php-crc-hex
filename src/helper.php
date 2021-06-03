6bad28afe8434827f66f2169ff27df7107f8045f
@小小只^v^ <littlezov@qq.com>
2021-06-04 04:17:06 +0800

    调整函数助手

--- src/helper.php
+++ src/helper.php
@@ -13,35 +13,40 @@ declare(strict_types=1);
  * @license  https://github.com/littlezo/MozillaPublicLicense/blob/main/LICENSE
  *
  */
-function hexScreen($hexStr, $is_hex = true)
-{
-    $str = '';
-    $hexStrArr = [];
-    for ($i = 0, $j = 1; $i < strlen($hexStr); $i = $i + 2, $j++) {
-        $hexStrArr[] = $hexStr[$i] . $hexStr[$i + 1];
+if (function_exists('hexScreen')) {
+    function hexScreen($hexStr, $is_hex = true)
+    {
+        $str = '';
+        $hexStrArr = [];
+        for ($i = 0, $j = 1; $i < strlen($hexStr); $i = $i + 2, $j++) {
+            $hexStrArr[] = $hexStr[$i] . $hexStr[$i + 1];
+        }
+        return $is_hex == true ? implode(' ', $hexStrArr) : implode('', $hexStrArr);
     }
-    return $is_hex == true ? implode(' ', $hexStrArr) : implode('', $hexStrArr);
 }
-
-function hexToStr($hex)
-{
-    $str = '';
-    for ($i = 0; $i < strlen($hex) - 1; $i += 2) {
-        $str .= chr(hexdec($hex[$i] . $hex[$i + 1]));
+if (function_exists('hexToStr')) {
+    function hexToStr($hex)
+    {
+        $str = '';
+        for ($i = 0; $i < strlen($hex) - 1; $i += 2) {
+            $str .= chr(hexdec($hex[$i] . $hex[$i + 1]));
+        }
+        return $str;
     }
-    return $str;
 }
-
-function strToHex($str)
-{
-    $hex = '';
-    for ($i = 0; $i < strlen($str); ++$i) {
-        $hex .= dechex(ord($str[$i]));
+if (function_exists('strToHex')) {
+    function strToHex($str)
+    {
+        $hex = '';
+        for ($i = 0; $i < strlen($str); ++$i) {
+            $hex .= dechex(ord($str[$i]));
+        }
+        return $hex;
     }
-    return $hex;
 }
-
-function isHexString($str)
-{
-    return ctype_xdigit($str);
+if (function_exists('isHexString')) {
+    function isHexString($str)
+    {
+        return ctype_xdigit($str);
+    }
 }
